76ddd1d266a3e9dc243bfd75907d708170f9faf9
ronalgp <78443892+ronalgp@users.noreply.github.com>
2025-07-24 01:56:32 +0200

    End of section 6

---
+++ client/src/core/services/toast-service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToastService {
+  constructor() {
+    this.createToastContainer();
+  }
+
+  private createToastContainer(): void {
+    if (!document.getElementById('toast-container')) {
+      const toastContainer = document.createElement('div');
+      toastContainer.id = 'toast-container';
+      toastContainer.className = 'toast toast-bottom toast-end';
+      document.body.appendChild(toastContainer);
+    }
+  }
+
+  private createToastElement(
+    message: string,
+    alertClass: string,
+    duration = 5000
+  ) {
+    const toastContainer = document.getElementById('toast-container');
+    if (!toastContainer) return;
+
+    const toast = document.createElement('div');
+    toast.classList.add('alert', alertClass, 'shadow-lg');
+    toast.innerHTML = `
+    <span>${message}</span>
+    <button class="ml-4 btn-sm btn-ghost">x</button>
+    `;
+
+    toast.querySelector('button')?.addEventListener('click', () => {
+      toastContainer.removeChild(toast);
+    });
+
+    toastContainer.append(toast);
+
+    setTimeout(() => {
+      if (toastContainer.contains(toast)) {
+        toastContainer.removeChild(toast);
+      }
+    }, duration);
+  }
+
+  success(message: string, duration?: number): void {
+    this.createToastElement(message, 'alert-success', duration);
+  }
+
+  error(message: string, duration?: number): void {
+    this.createToastElement(message, 'alert-error', duration);
+  }
+
+  warning(message: string, duration?: number): void {
+    this.createToastElement(message, 'alert-warning', duration);
+  }
+
+  info(message: string, duration?: number): void {
+    this.createToastElement(message, 'alert-info', duration);
+  }
+}
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,30 +1,38 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
+  private router = inject(Router);
+  private toast = inject(ToastService);
   protected creds: any = {};
   protected loggedIn = signal(false);
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: (result) => {
-        console.log('Login successful', result);
+      next: () => {
+        this.router.navigateByUrl('/members');
         this.creds = {};
+        this.toast.success("Login successfully.", 5000);
       },
-      error: (error) => console.error('Login failed', error),
+      error: (error) => {
+        console.error('Login failed', error);
+        this.toast.error(error.error);
+      },
     });
   }
 
   logout() {
     this.accountService.logout();
-    console.log('Logged out');
+    this.router.navigateByUrl('/');
   }
 }
